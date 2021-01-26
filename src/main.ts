cfff57a3f2fc63f3a35412a234a905fc2fcced7e
khotsevich <xozewitc@yandex.ru>
2021-01-26 22:25:41 +0300

    Create task ownership and learning logging concept

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = 3000;
+  const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
+  logger.log(`Application listening on port ${port}`);
 }
 bootstrap();
